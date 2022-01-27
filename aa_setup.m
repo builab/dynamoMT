12b2db52e744b9a8cf4b38421f4451d8e5f96c14
builab <huy.bui@mcgill.ca>
2022-01-27 14:11:25 -0500

    Update aa_setup.m

--- aa_setup.m
+++ aa_setup.m
@@ -2,12 +2,13 @@
 % Set up the project
 %%%%%%%%%%%%%%%%%%%%
 % Import tomogram & create catalog using the GUI
-catPath = '../catalogues/c001';
+catPath = '../catalogs/c001';
 docFilePath = '../catalogues/tomograms.doc'; % This has to be consistent with the vll after catalog creation
 vllFilePath = '../catalogues/tomograms.vll';
 
 % If using command line
-%dcm -create c001 -fromvll myList.vll
+cd catalogs
+dcm -create c001 -fromvll tomograms.vll
 
 % Import coordinate picked from IMOD using filament torsion model
 % Using GUI https://wiki.dynamo.biozentrum.unibas.ch/w/index.php/Filament_model
