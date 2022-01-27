f7c9b67167ffee940427e6f3faa30770ff3fde51
builab <huy.bui@mcgill.ca>
2022-01-27 16:18:32 -0500

    Create createInitialAvg.m

---
+++ createInitialAvg.m
@@ -0,0 +1,13 @@
+% Script to generate initial average
+
+% Input
+filamentListFile = 'filamentList.csv';
+particleDir = 'particles';
+
+
+
+
+
+filamentList = readcell(filamentListFile);
+
+% Generate initial average
