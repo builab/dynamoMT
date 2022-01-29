86065bac8e90e2041abde1d63c32c22578ef6a61
builab <huy.bui@mcgill.ca>
2022-01-28 23:02:23 -0500

    Update aa_alignIntraAvg.m

--- aa_alignIntraAvg.m
+++ aa_alignIntraAvg.m
@@ -59,7 +59,7 @@ dvput(pr_a,'limm',[1 2]);
 dvput(pr_a,'sym', 'c1');
 
 % set computational parameters
-dvput(pr_a,'dst','matlab_gpu','cores',2,'mwa',mw);
+dvput(pr_a,'dst','matlab_gpu','cores',1,'mwa',mw);
 dvput(pr_a,'gpus',gpu);
 
 % check/unfold/run
