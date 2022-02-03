655829eaf440a8c8e244eefc25629c11a8e1be2b
builab <huy.bui@mcgill.ca>
2022-02-03 00:29:58 -0500

    Update aa_repickParticles.m

--- aa_repickParticles.m
+++ aa_repickParticles.m
@@ -1,1 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Script to use an aligned table to repick the particles for accurate alignment
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Input the table
+
+% Repick
+
+% How to pick the particles at the right place
