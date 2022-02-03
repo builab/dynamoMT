27f4dc25951a5e8364ddb2b633043e7fa3ac6778
builab <huy.bui@mcgill.ca>
2022-02-03 00:19:55 -0500

    Update aa_alignIntraAvg.m

--- aa_alignIntraAvg.m
+++ aa_alignIntraAvg.m
@@ -5,31 +5,34 @@
 
 filamentListFile = 'filamentList.csv';
 alnDir = 'intraAln';
-particleDir = 'particles';
+outputDir = 'alnTable';
 mw = 12; % Number of parallel workers to run
 gpu = [0:1]; % Alignment using gpu
-folderAllaverages = 'avg_aln'; % Output alignment folder
-pr_a = 'pr_avg_aln'; % Project name
 boxSize = 96;
-templateFile = 'dmt_init_avg_b96.em';
+initRefFile = 'dmt_init_avg_b96.em';
 
 
 filamentList = readcell(filamentListFile);
 noFilament = length(filamentList);
+template = dread(initRefFile);
 
-
-template = dread(templateFile);
+cd(alnDir)
+mkdir(outputDir)
 
 % Need to apply transformation into original table
 for idx = 1:noFilament
-  aPath = ddb([filamentList{idx} ':a']);
+  aPath = ddb([filamentList{idx} ':a']); % Read the path of the alignment project average
+  tPath = ddb([filamentList{idx} ':rt']);
   filamentAvg = dread(aPath);
-  sal = dalign(dynamo_bandpass(template,[1 23]), ...
-  dynamo_bandpass(filamentAvg,[1 23]),'cr',1,'cs',1,'ir',1,'is',1,'dim',96, 'limm',1,'lim',[1,1,20],'rf',5,'rff',2); % cone_flip
+  sal = dalign(dynamo_bandpass(template,[1 23]), dynamo_bandpass(filamentAvg,[1 23]),'cr',14,'cs',2,'ir',360,'is',2,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2); % cone_flip
+  %sal = dalign(dynamo_bandpass(template,[1 23]), dynamo_bandpass(filamentAvg,[1 23]),'cr',14,'cs',2,'ir',360,'is',2,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2, 'cone_flip', 1); % cone_flip
   dview(sal.aligned_particle);
   % Read last table from alignment
-  t_ccFilt_Ex = dread(['t_ccFilt_Ex_' stackName{idx} '.tbl']);
+  tFilament = dread(tPath);
   % Read last transformation & applied to table
-  t_ccFilt_Ex_Ali = dynamo_table_rigid(t_ccFilt_Ex,sal.Tp);
+  tFilament_ali = dynamo_table_rigid(tFilament, sal.Tp);
   % Write table
+  dwrite(tFilament_ali, [outputDir '/' filamentList{idx} '.tbl'])
  end
+ 
+ cd ..
