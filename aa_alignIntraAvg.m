96a6f9d93a8f7f3f1885ad38576b2ca4015f1eed
builab <huy.bui@mcgill.ca>
2022-03-01 23:23:05 -0500

    Update aa_alignIntraAvg.m

--- aa_alignIntraAvg.m
+++ aa_alignIntraAvg.m
@@ -29,9 +29,9 @@ for idx = 1:noFilament
 	tPath = ddb([filamentList{idx} ':rt']);
 	filamentAvg = dread(aPath);
 	if coneFlip > 0
-  		sal = dalign(dynamo_bandpass(template,[1 lowpass]), dynamo_bandpass(filamentAvg,[1 lowpass]),'cr',15,'cs',5,'ir',360,'is',10,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2, 'cone_flip', 1); % cone_flip
+  		sal = dalign(dynamo_bandpass(filamentAvg,[1 lowpass]), dynamo_bandpass(template,[1 lowpass]),'cr',15,'cs',5,'ir',360,'is',10,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2, 'cone_flip', 1); % cone_flip
 	else
-  		sal = dalign(dynamo_bandpass(template,[1 lowpass]), dynamo_bandpass(filamentAvg,[1 lowpass]),'cr',15,'cs',5,'ir',360,'is',10,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2); % no cone_flip
+  		sal = dalign(dynamo_bandpass(filamentAvg,[1 lowpass]), dynamo_bandpass(template,[1 lowpass]),'cr',15,'cs',5,'ir',360,'is',10,'dim',96, 'limm',1,'lim',[20,20,20],'rf',5,'rff',2); % no cone_flip
 	end
 	dview(sal.aligned_particle);
 	% Read last table from alignment
