a0243ad5ea7cac7ecb2d95abc03fee357b4c716a
builab <huy.bui@mcgill.ca>
2022-05-09 22:34:08 -0400

    v0.11

--- aa_filament_repick.m
+++ aa_filament_repick.m
@@ -9,6 +9,7 @@
 % NOTE: Important to have tomogram number 
 % NOTE: If the filament twist (microtubule/CP), we need to define subunits_dphi to describe the torsion.
 % however, it might be related to the polarity of the filament (- or + sign).
+% Perhaps, should use a nearest neighbour guessing of angle
 
 %%%%%%%% Before Running Script %%%%%%%%%%
 %%% Activate Dynamo
@@ -21,66 +22,63 @@ prjPath = '/london/data0/20220404_TetraCU428_Tip_TS/ts/tip_CP_dPhi/';
 
 % Input
 docFilePath = sprintf('%scatalogs/tomograms.doc', prjPath);
-modelDir = sprintf('%smodels', prjPath);
+modelDir = sprintf('%smodels_repick', prjPath);
+particleDir = sprintf('%sparticles_repick', prjPath);
 c001Dir = sprintf('%scatalogs/c001', prjPath);
 pixelsize = 8.48; % Angstrom per pixel
 periodicity = 82.8; % Using 16-nm of doublet for DMT 
+boxSize = 96;
 subunits_dphi = 0.72;  % For the tip CP
 subunits_dz = periodicity/pixelsize; % in pixel repeating unit dz = 8.4 nm = 168 Angstrom/pixelSize
 filamentListFile = sprintf('%sfilamentList.csv', prjPath);
+tableAlnFileName = 'merged_particles_aln.tbl'; % merge particles after alignment
+
 
 % loop through all tomograms
 fileID = fopen(docFilePath); D = textscan(fileID,'%d %s'); fclose(fileID);
 tomoID = D{1,1}'; % get tomogram ID
 nTomo = length(D{1,2}); % get total number of tomograms
 
-filamentList = {};
+tblAll = dread(tableAlnFileName);
 
 % Loop through tomograms
 for idx = 1:nTomo
     tomo = D{1,2}{idx,1};
     [tomoPath,tomoName,ext] = fileparts(tomo);
+    tomono = D{1,1}(idx);
     % Modify specific to name
     tomoName = strrep(tomoName, '_rec', ''); % Remove the rec part of the name
-    imodModel = [modelDir '/' tomoName '.txt'];
+    tableTomo = tblAll(tblAll(:,20) == tomono, :);
+    
     modelout = strrep(imodModel, '.txt', '.omd');
+    points = tableTomo(:, 24:26) + tableTomo(:, 4:6);
     
-    allpoints = load(imodModel);
-    
+    contour = [1];
+    % Compatible with doublet microtubule
     m = {}; % Cell array contains all filament
-    contour = unique(allpoints(:, 1));
-    % Loop through filaments
-    for i = 1:length(contour)
-        filamentid = contour(i);
-        points = allpoints(allpoints(:, 1) == filamentid, 2:4);
-        m{i} = dmodels.filamentWithTorsion();
-        m{i}.subunits_dphi = subunits_dphi;
-        m{i}.subunits_dz = subunits_dz;
-        m{i}.name = [tomoName '_' num2str(contour(i))];
-        % Import coordinate
-        m{i}.points = points;
-        % Create backbone
-        m{i}.backboneUpdate();
-        % Update crop point (can change dz)
-        m{i}.updateCrop();
-        % Link to catalog
-        m{i}.linkCatalogue(c001Dir, 'i', idx);
-        m{i}.saveInCatalogue();
+	for i = 1:length(contour)
+   	 	m{i} = dmodels.filamentWithTorsion();
+    	m{i}.subunits_dphi = subunits_dphi;
+    	m{i}.subunits_dz = subunits_dz;
+    	m{i}.name = [tomoName '_' num2str(contour(i))];
+    	% Import coordinate
+    	m{i}.points = points;
+    	% Create backbone
+   	 	m{i}.backboneUpdate();
+   	 	% Update crop point (can change dz)
+    	m{i}.updateCrop();
+    	% Link to catalog
+    	m{i}.linkCatalogue(c001Dir, 'i', idx);
+    	m{i}.saveInCatalogue();
         
-        % Add to the list
-        filamentList{end + 1, 1} = [tomoName '_' num2str(contour(i))];
-
-        % Testing this block
-        t = m{i}.grepTable();
-        dwrite(t, [modelDir '/' tomoName '_' num2str(contour(i)) '.tbl']);
-        % Optional for visualization of table
-        %dtplot(['particles/' tomoName '_' num2str(contour(i)) '/crop.tbl'], 'pf', 'oriented_positions');
-    end
-    
+    	t = m{i}.grepTable();
+    	dwrite(t, [modelDir '/' tomoName '_' num2str(contour(i)) '.tbl']);
+    	targetFolder = [particleDir '/'  tomoName '_' num2str(contour(i))];
+  		% Cropping subtomogram out
+  		dtcrop(docFilePath, t, targetFolder, boxSize, 'mw', mw);
+  		oa = daverage(targetFolder, 't', t, 'fc', 1, 'mw', mw);
+  		dwrite(dynamo_bandpass(oa.average, [1 lowpass]), [targetFolder '/template.em']);
+    end 
     % Write the DynamoModel
-    dwrite(m, modelout)
-
+    dwrite(m, modelout);
 end
-
-% Write out list file
-writecell(filamentList, filamentListFile);
