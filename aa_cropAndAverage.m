f6f4af4a735c0d2d8e588a8edfbaffb86a9d1ec8
builab <huy.bui@mcgill.ca>
2022-06-02 13:38:32 -0400

    Update aa_cropAndAverage.m

--- aa_cropAndAverage.m
+++ aa_cropAndAverage.m
@@ -48,5 +48,5 @@ for idx = 1:length(filamentList)
   dwrite(dynamo_bandpass(oa.average, [1 lowpass]), [targetFolder '/template.em']);
   dtplot([targetFolder '/crop.tbl'], 'pf', 'oriented_positions');
 	view(-230,30);axis equal;
-	print([targetFolder '/pick_' tomoName] , '-dpng');
+	print([targetFolder '/pick_' filamentList{idx}] , '-dpng');
 end
