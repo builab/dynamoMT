6d87596c3e7bfd3b9503d34de7458fa5d0cc1d50
Builab <huy.bui@mcgill.ca>
2022-06-19 23:37:34 -0400

    Update aa_alignRepickAvg.m

--- aa_alignRepickAvg.m
+++ aa_alignRepickAvg.m
@@ -3,7 +3,8 @@
 % and transform all the alignment to an updated table.
 % dynamoDMT v0.2b
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% This might be changed to accommodate the intraAlnRepick
+%
 %%%%%%%% Before Running Script %%%%%%%%%%
 %%% Activate Dynamo
 run /london/data0/software/dynamo/dynamo_activate.m
@@ -16,6 +17,7 @@ pixelSize = 8.48;
 boxSize = 96;
 filamentListFile = 'filamentList.csv';
 particleDir = sprintf('%sparticles_repick', prjPath);
+alnDir = sprintf('%sintraAln_repick', prjPath);
 previewDir =[particleDir '/preview']; % created from previously
 mw = 12; % Number of parallel workers to run
 gpu = [0:5]; % Alignment using gpu
@@ -35,13 +37,20 @@ template = dread(initRefFile);
 
 alnLowpassPix = round(pixelSize/alnLowpass*boxSize);
 mkdir(previewDir)
+%v0.2b
+% Need to go into alnDir to read the intraAln project
+cd(alnDir)
+
 % Calculate the alignment of the filamentAverage to the initial reference
 % transform the corresponding table for all particles
 for idx = 1:noFilament
-	aPath = ([particleDir '/' filamentList{idx} '/template.em']); % Read the path of the alignment project average
-	tPath = ([particleDir '/' filamentList{idx} '/crop.tbl']); 
+	% v0.2b
+	%aPath = ([particleDir '/' filamentList{idx} '/template.em']); % Read the path of the alignment project average
+	%tPath = ([particleDir '/' filamentList{idx} '/crop.tbl']); 
+	aPath = ddb([filamentList{idx} ':a']); % Read the path of the alignment project average
+	tPath = ddb([filamentList{idx} ':rt']);
 	filamentAvg = dread(aPath);
-    disp(filamentList{idx})
+    	disp(filamentList{idx})
 	if coneFlip > 0
   		sal = dalign(dynamo_bandpass(filamentAvg,[1 alnLowpassPix]), dynamo_bandpass(template,[1 alnLowpassPix]),'cr',10,'cs',5,'ir',360,'is',5,'dim',boxSize, 'limm',1,'lim',[5,5,zshift_limit],'rf',5,'rff',2, 'cone_flip', 1); % cone_flip
 	else
@@ -58,7 +67,7 @@ for idx = 1:noFilament
 	tFilament = dread(tPath);
 	% Read last transformation & applied to table
 	tFilament_ali = dynamo_table_rigid(tFilament, sal.Tp);
-	% Write tablev
+	% Write table
 	dwrite(tFilament_ali, [particleDir '/' filamentList{idx} '/aligned.tbl'])
 	
 
