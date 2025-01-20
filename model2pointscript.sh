a9805bd255d8a39d51bb4b8a229ded3719d4826e
builab <huy.bui@mcgill.ca>
2025-01-20 17:30:14 -0500

    Update model2pointscript.sh

--- model2pointscript.sh
+++ model2pointscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# model2pointscript.sh modDir modelDir modelfile stringToBeRemoved
 
 module load imod
 
@@ -8,9 +9,10 @@ cat $3 | while read line || [[ -n $line ]];
 do
     a=$(dirname $line)
     echo "$a"
-    # suffix=$4
-    # echo "$suffix"
-	# b=$(echo "$line" | sed -e "s$suffix$//")
-    # echo "$b"
-	model2point -Contour $1$line $2$a.txt
+    suffix=$4
+    echo "$suffix"
+    b=$(echo "$line" | sed -e "s/$suffix$//")
+    echo "$b"
+    model2point -Contour $1$line $2$b.txt
+    echo "model2point -Contour $1$line $2$b.txt"
 done
